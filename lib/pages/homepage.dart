8e065e20270c2f97f69cf434138b11645907d69f
anisha-n12 <nemadeanisha04@gmail.com>
2024-01-08 15:42:43 +0530

    bottomnavigation

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,33 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex=0;
+
+  void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+  });
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         elevation: 0,
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text(
           "Home",
           style: TextStyle(
-              fontSize: 27, fontWeight: FontWeight.bold, color: Colors.black),
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
         ),
       ),
-    body:Column(children: [
-        Container(
-          child: ElevatedButton(
-            onPressed:(){},
-            style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        foregroundColor: Colors.white),
-            child: const Text("Login"),
-    ),),
-    Container(
-      child: ListView.builder(itemBuilder: (context,index){
-        return Marquee(text: "hi");
-      },)
-    ),]),);
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                const SizedBox(width: 320,),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                  ),
+                  child: const Text("Login"),
+                ),
+              ],
+            ),
+            
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 15,
+        selectedIconTheme: const IconThemeData(color: Colors.black, size: 20),
+        selectedItemColor: Colors.black,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+        currentIndex: _selectedIndex, //New
+        onTap: _onItemTapped, 
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Theme.of(context).primaryColor,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article_outlined),
+            label: 'Forms',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on_outlined),
+            label: 'Maps',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.call_outlined),
+            label: 'Contact',
+          ),
+        ],
+  ),
+    );
+    
   }
 }
