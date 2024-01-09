7d95fac006e63aac41052e7b1146156156be5d24
anisha-n12 <nemadeanisha04@gmail.com>
2024-01-09 14:06:36 +0530

    anisha

--- lib/pages/homeinfo.dart
+++ lib/pages/homeinfo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeInfo extends StatefulWidget {
   const HomeInfo({super.key});
@@ -27,9 +28,9 @@ class _HomeInfoState extends State<HomeInfo> {
           ),
           Container(
               child: Column(
+            children: [
+              Row(
                 children: [
-                  Row(
-                              children: [
                   ButtonBar(
                     buttonPadding: const EdgeInsets.all(10),
                     alignment: MainAxisAlignment.center,
@@ -44,9 +45,13 @@ class _HomeInfoState extends State<HomeInfo> {
                         style: ElevatedButton.styleFrom(
                             backgroundColor:
                                 _currentIndex == 0 ? Colors.black : Colors.grey,
-                            foregroundColor:
-                                _currentIndex == 0 ? Colors.white : Colors.black),
-                        child: const Text("Photos", style: TextStyle(fontSize: 13),),
+                            foregroundColor: _currentIndex == 0
+                                ? Colors.white
+                                : Colors.black),
+                        child: const Text(
+                          "Photos",
+                          style: TextStyle(fontSize: 13),
+                        ),
                       ),
                       ElevatedButton(
                         onPressed: () {
@@ -57,9 +62,13 @@ class _HomeInfoState extends State<HomeInfo> {
                         style: ElevatedButton.styleFrom(
                             backgroundColor:
                                 _currentIndex == 1 ? Colors.black : Colors.grey,
-                            foregroundColor:
-                                _currentIndex == 1 ? Colors.white : Colors.black),
-                        child: const Text("Facilities", style: TextStyle(fontSize: 13),),
+                            foregroundColor: _currentIndex == 1
+                                ? Colors.white
+                                : Colors.black),
+                        child: const Text(
+                          "Facilities",
+                          style: TextStyle(fontSize: 13),
+                        ),
                       ),
                       ElevatedButton(
                         onPressed: () {
@@ -70,9 +79,13 @@ class _HomeInfoState extends State<HomeInfo> {
                         style: ElevatedButton.styleFrom(
                             backgroundColor:
                                 _currentIndex == 2 ? Colors.black : Colors.grey,
-                            foregroundColor:
-                                _currentIndex == 2 ? Colors.white : Colors.black),
-                        child: const Text("Mess", style: TextStyle(fontSize: 13),),
+                            foregroundColor: _currentIndex == 2
+                                ? Colors.white
+                                : Colors.black),
+                        child: const Text(
+                          "Mess",
+                          style: TextStyle(fontSize: 13),
+                        ),
                       ),
                       ElevatedButton(
                         onPressed: () {
@@ -83,20 +96,23 @@ class _HomeInfoState extends State<HomeInfo> {
                         style: ElevatedButton.styleFrom(
                             backgroundColor:
                                 _currentIndex == 3 ? Colors.black : Colors.grey,
-                            foregroundColor:
-                                _currentIndex == 3 ? Colors.white : Colors.black),
-                        child: const Text("Blocks", style: TextStyle(fontSize: 13),),
+                            foregroundColor: _currentIndex == 3
+                                ? Colors.white
+                                : Colors.black),
+                        child: const Text(
+                          "Blocks",
+                          style: TextStyle(fontSize: 13),
+                        ),
                       ),
                     ],
                   ),
-                  
-                              ],
-                            ),
-                  Center(
+                ],
+              ),
+              Center(
                 child: _tabs[_currentIndex],
               )
-                ],
-              )),
+            ],
+          )),
         ],
       ),
     );
@@ -113,7 +129,92 @@ class PhotoGallery extends StatefulWidget {
 class _PhotoGalleryState extends State<PhotoGallery> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Photos"));
+    return const Center(child:Text("hi"));
+    // ListView(
+    //   scrollDirection: Axis.horizontal,
+    //   children: [
+    //     CarouselSlider(
+    //       items: [
+    //         //1st Image of Slider
+    //         Container(
+    //           margin: EdgeInsets.all(6.0),
+    //           // decoration: BoxDecoration(
+    //           //   borderRadius: BorderRadius.circular(8.0),
+    //           //   image: DecorationImage(
+    //           //     image: AssetImage("assets/hostel.jpeg"),
+    //           //     fit: BoxFit.cover,
+    //           //   ),
+    //           // ),
+    //           child: Center(child:Text("hi")),
+    //         ),
+    
+    //         //2nd Image of Slider
+    //         Container(
+    //           margin: EdgeInsets.all(6.0),
+    //           // decoration: BoxDecoration(
+    //           //   borderRadius: BorderRadius.circular(8.0),
+    //           //   image: DecorationImage(
+    //           //     image: AssetImage("assets/hostel.jpeg"),
+    //           //     fit: BoxFit.cover,
+    //           //   ),
+    //           // ),
+    //           child: Center(child:Text("hi")),
+    //         ),
+    
+    //         //3rd Image of Slider
+    //         Container(
+    //           margin: EdgeInsets.all(6.0),
+    //           // decoration: BoxDecoration(
+    //           //   borderRadius: BorderRadius.circular(8.0),
+    //           //   image: DecorationImage(
+    //           //     image: AssetImage("assets/hostel.jpeg"),
+    //           //     fit: BoxFit.cover,
+    //           //   ),
+    //           // ),
+    //           child: Center(child:Text("hi")),
+    //         ),
+    
+    //         //4th Image of Slider
+    //         Container(
+    //           margin: EdgeInsets.all(6.0),
+    //           // decoration: BoxDecoration(
+    //           //   borderRadius: BorderRadius.circular(8.0),
+    //           //   image: DecorationImage(
+    //           //     image: AssetImage("assets/hostel.jpeg"),
+    //           //     fit: BoxFit.cover,
+    //           //   ),
+    //           // ),
+    //           child: Center(child:Text("hi")),
+    //         ),
+    
+    //         //5th Image of Slider
+    //         Container(
+    //           margin: EdgeInsets.all(6.0),
+    //           // decoration: BoxDecoration(
+    //           //   borderRadius: BorderRadius.circular(8.0),
+    //           //   image: DecorationImage(
+    //           //     image: AssetImage("assets/hostel.jpeg"),
+    //           //     fit: BoxFit.cover,
+    //           //   ),
+    //           // ),
+    //           child: Center(child:Text("hi")),
+    //         ),
+    //       ],
+    
+    //       //Slider Container properties
+    //       options: CarouselOptions(
+    //         height: 180.0,
+    //         enlargeCenterPage: true,
+    //         autoPlay: true,
+    //         aspectRatio: 16 / 9,
+    //         autoPlayCurve: Curves.fastOutSlowIn,
+    //         enableInfiniteScroll: true,
+    //         autoPlayAnimationDuration: Duration(milliseconds: 800),
+    //         viewportFraction: 0.8,
+    //       ),
+    //     ),
+    //   ],
+    // );
   }
 }
 
